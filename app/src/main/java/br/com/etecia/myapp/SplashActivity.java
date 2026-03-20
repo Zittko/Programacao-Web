ee5714e6225932a623afb6fcd68a623ac679cddb
Zittko <daniel.y.saiki@gmail.com>
2026-03-19 22:21:39 -0300

    update

--- app/src/main/java/br/com/etecia/myapp/SplashActivity.java
+++ app/src/main/java/br/com/etecia/myapp/SplashActivity.java
@@ -2,6 +2,7 @@ package br.com.etecia.myapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -33,10 +34,18 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-
                 finish();
             }
         });
+        // Abrir um janela em processo background
+        Handler hd = new Handler();
 
+        hd.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        }, 3000);
     }
 }
